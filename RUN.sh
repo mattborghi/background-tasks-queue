f92813d3045b649ed60a717ebb4c76973e13a6f9
Matias Borghi <borghi.matias@gmail.com>
2021-01-12 10:41:05 -0300

    add RUN script

--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+# Get args
+WORKERS=1 # set a default
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+    -w | --workers)
+        WORKERS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    esac
+done
+
 # RUN WORKERS AND SINK
+for ((i = 1; i <= $WORKERS; i++)); do
+    gnome-terminal --tab --title="Worker $i" -- bash -c "julia worker.jl"
+done
+
+gnome-terminal --tab --title="Sink" -- bash -c "cd results; julia ./sink.jl"
 
 # RUN BACKEND
+gnome-terminal --tab --title="Backend" -- bash -c "cd backend; pipenv shell --anyway 'python manage.py runserver'"
 
 # RUN FRONTEND
+# gnome-terminal --tab --title="Frontend" -- bash -c "cd frontend; npm run start"
