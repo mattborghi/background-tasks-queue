178e8e6cd9ef376bb8fb00d86d35da7b423de607
Matias Borghi <borghi.matias@gmail.com>
2021-01-15 10:39:18 -0300

    improved script if user writes wrong input arguments

--- RUN.sh
+++ RUN.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Get args
+# PARSE INPUT ARGUMENTS
+
 WORKERS=1 # set a default
 
 while [[ $# -gt 0 ]]; do
@@ -12,15 +13,21 @@ while [[ $# -gt 0 ]]; do
         shift # past argument
         shift # past value
         ;;
+    *)
+        echo "Ignoring input argument: $key"
+        shift # Shift removes from the list the argument
+        shift # and value so we can continue with the next
+        ;;
     esac
 done
 
-# RUN WORKERS AND SINK
+# RUN WORKERS
 for ((i = 1; i <= $WORKERS; i++)); do
-    gnome-terminal --tab --title="Worker $i" -- bash -c "cd results; julia --project="." src/worker.jl"
+    gnome-terminal --tab --title="Worker $i" -- bash -c "cd results; julia --project="Worker" run_worker.jl"
 done
 
-gnome-terminal --tab --title="Sink" -- bash -c "cd results; julia --project="." src/sink.jl"
+# RUN SINK
+gnome-terminal --tab --title="Sink" -- bash -c "cd results; julia --project="Sink" run_sink.jl"
 
 # RUN BACKEND
 gnome-terminal --tab --title="Backend" -- bash -c "cd backend; pipenv shell --anyway 'python manage.py runserver'"
