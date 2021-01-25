cb7c02908eeea4d5fc8da8b0ccc0ff137e791ec4
Matias Borghi <borghi.matias@gmail.com>
2021-01-25 20:29:47 -0300

    add new proxy stack from new design load balancer design pattern

---
+++ results/Proxy/main.py
@@ -0,0 +1,8 @@
+from LoadBalancer.controller import Controller
+
+
+def run():
+    print("Running Proxy")
+    # controller =
+    Controller(debug=True)
+    # print(controller.statistics)
---
+++ results/run_proxy.py
@@ -0,0 +1,4 @@
+from Proxy.main import run
+
+if __name__ == "__main__":
+    run()
