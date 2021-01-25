4c22904f3fd89120e2b295d3f10e13e317399333
Matias Borghi <borghi.matias@gmail.com>
2021-01-25 20:35:46 -0300

    improved backend command + added new proxy command

--- RUN.sh
+++ RUN.sh
@@ -29,8 +29,11 @@ done
 # RUN SINK
 gnome-terminal --tab --title="Sink" -- bash -c "cd results; julia --project="Sink" run_sink.jl"
 
+# RUN PROXY
+gnome-terminal --tab --title="Proxy" -- bash -c "cd results; PIPENV_PIPFILE=./Proxy/Pipfile pipenv run python run_proxy.py"
+
 # RUN BACKEND
-gnome-terminal --tab --title="Backend" -- bash -c "cd backend; pipenv shell --anyway 'python manage.py runserver'"
+gnome-terminal --tab --title="Backend" -- bash -c "cd backend; pipenv run python manage.py runserver"
 
 # RUN FRONTEND
 gnome-terminal --tab --title="Frontend" -- bash -c "cd frontend; npm run start"
