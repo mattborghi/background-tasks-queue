600e343cba01f3a4426008e9e570d0b85e22aae2
Matias Borghi <borghi.matias@gmail.com>
2021-01-29 16:15:49 -0300

    removed unnecessary files

--- results/Proxy/main.py
+++
@@ -1,8 +0,0 @@
-from LoadBalancer.controller import Controller
-
-
-def run():
-    print("Running Proxy")
-    # controller =
-    Controller(debug=True)
-    # print(controller.statistics)
--- results/run_proxy.py
+++
@@ -1,4 +0,0 @@
-from Proxy.main import run
-
-if __name__ == "__main__":
-    run()
