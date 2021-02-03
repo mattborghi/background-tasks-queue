ce1b6574ae8caa1a2e0ce482caf7a2d1a2fbb8f5
Matias Borghi <borghi.matias@gmail.com>
2021-02-03 12:34:54 -0300

    too many redirects for ssl now

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Make http -> https redicrects
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
