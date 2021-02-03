62473a3d4bed1a2a6d8b32d22c2bb8c130736dc5
Matias Borghi <borghi.matias@gmail.com>
2021-02-03 12:27:07 -0300

    add auto redirect http->https to django

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -124,6 +124,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Make http -> https redicrects
+SECURE_SSL_REDIRECT = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
