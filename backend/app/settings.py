7cb1a56c886e0ba9a37292d0fd4d57b486902c57
Matias Borghi <borghi.matias@gmail.com>
2021-02-04 10:12:35 -0300

    minor changes

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_BACKEND_KEY', default='foo')
-# SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
