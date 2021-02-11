9101bedaec896b5253b75ab301321c5a482c17fe
Matias Borghi <borghi.matias@gmail.com>
2021-02-11 13:22:04 -0300

    updated Julia Sink to work on heroku

--- results/Sink/src/Sink.jl
+++ results/Sink/src/Sink.jl
@@ -1,101 +1,90 @@
 #!/usr/bin/env julia
 
-#
-# Task sink
-# Binds PULL socket to tcp://localhost:5558
-# Collects results from workers via that socket
-#
-
 module Sink
 
 # Load packages
 using DotEnv
 using Diana
 using JSON
-using AMQPClient
+using PyCall
 
 include("graphql.jl")
 include("utils.jl")
 
+
+SINK_CHANNEL = "sink"
+
 # TODO: Use path relative to this file
-DotEnv.config(path="../.ENV")
+DotEnv.config(path="./.ENV")
+
+GRAPHQL_URL = haskey(ENV, "GRAPHQL_URL") ? ENV["GRAPHQL_URL"] : """http://$(ENV["HOST"]):$(ENV["PORT"])/$(ENV["CHANNEL"])"""
+
+@show GRAPHQL_URL
 
 abstract type CustomConnection end
 
 struct Connection <: CustomConnection
-    port 
-    login
-    password
-    conn
-    chan
+    pika
+    connection
+    channel
 end
 
 function connect()
     # Establish the connection to the RabbitMQ Server
-    port = AMQPClient.AMQP_DEFAULT_PORT
-    login = "guest"  # default is usually "guest"
-    password = "guest"  # default is usually "guest"
-    auth_params = Dict{String,Any}("MECHANISM" => "AMQPLAIN", "LOGIN" => login, "PASSWORD" => password)
+    pika = pyimport("pika")
     
+    parameters = haskey(ENV, "CLOUD_AMQP_URL") ? 
+                pika.URLParameters(ENV["CLOUD_AMQP_URL"]) : 
+                pika.ConnectionParameters(host="0.0.0.0")
+
+    @show parameters
+
     printstyledln("[🔌] Sink stablishing connection.";bold=true,color=:green)
     
-    conn = connection(; virtualhost="/", host="localhost", port=port, auth_params=auth_params, amqps=nothing)
+    connection = pika.BlockingConnection(parameters)
 
-    chan = channel(conn, AMQPClient.UNUSED_CHANNEL, true)
+    channel = connection.channel()
     
-    return Connection(port, login, password, conn, chan)
+    return Connection(pika, connection, channel)
 end
-
-URL = """http://$(ENV["HOST"]):$(ENV["PORT"])/$(ENV["CHANNEL"])"""
 
 create_vars = (resultId, value) -> Dict("resultId" => resultId, "value" => value)
 create_status = (resultId, status) -> Dict("resultId" => resultId, "status" => status)
 
 function run_sink(connection::CustomConnection)
-    chan = connection.chan
+    # TODO: Use Parameters.jl's @unpack?
+    channel = connection.channel
 
-    SINK_CHANNEL = "sink"
-
-    success, message_count, consumer_count = queue_declare(chan, SINK_CHANNEL, durable=true)
+    # Declare queue to receive results from client
+    channel.queue_declare(queue=SINK_CHANNEL, durable=true)
     
     printstyledln("[⏳] Waiting for messages. To exit press CTRL+C";bold=true,color=:green)
 
-    callback = rcvd_msg -> begin
-        msg_str = JSON.parse(String(rcvd_msg.data))
+    callback = (ch, method, properties, body) -> begin
+        message = JSON.parse(String(body))
         
         printstyledln("[📦] Received from Worker:";bold=true,color=:green) 
-        tabulate_and_pretty(JSON.json(msg_str, 4))
+        tabulate_and_pretty(JSON.json(message, 4))
 
-        id = msg_str["id"]
+        id = message["id"]
         
-        if msg_str["status"] == "FINISHED"
-            value = msg_str["result"]
-            _ = Queryclient(URL, UPDATE_RESULT; vars=create_vars(id, value))
-        elseif msg_str["status"] == "FAILED"
-            _ = Queryclient(URL, UPDATE_RESULT_STATUS; vars=create_status(id, "FAILED"))
+        if message["status"] == "FINISHED"
+            value = message["result"]
+            _ = Queryclient(GRAPHQL_URL, UPDATE_RESULT; vars=create_vars(id, value))
+        elseif message["status"] == "FAILED"
+            _ = Queryclient(GRAPHQL_URL, UPDATE_RESULT_STATUS; vars=create_status(id, "FAILED"))
         else
             error("Status not handled")
         end
         # It's time to remove the auto_ack flag and 
         # send a proper acknowledgment from the worker, 
         # once we're done with a task.
-        basic_ack(chan, rcvd_msg.delivery_tag)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
     end
     
-    success, consumer_tag = basic_consume(chan, SINK_CHANNEL, callback)
+    channel.basic_consume(queue=SINK_CHANNEL, on_message_callback=callback)
     
-    @assert success
-    # println("consumer registered with tag $consumer_tag")
-
-    # go ahead with other stuff...
-    # or wait for an indicator for shutdown
-
-    while true
-        sleep(1)
-    end
-    
-    return nothing
-
+    channel.start_consuming()            
 end
 
 export connect, run_sink, printstyledln
