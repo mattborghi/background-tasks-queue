729aa6d9164d9f0412c3c16f85650567e137d8f5
Matias Borghi <borghi.matias@gmail.com>
2021-02-12 12:53:11 -0300

    add necessary modifications to backend to use the code given by the user

--- backend/results/models.py
+++ backend/results/models.py
@@ -7,6 +7,7 @@ class Result(models.Model):
     #auto id field
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    code = models.TextField(null=False, default="")
     # created_by = models.ForeignKey('contracts.Contract', related_name='contract_of_result', on_delete=models.CASCADE)
     value = models.FloatField(null=True)
     status = models.CharField(max_length=8, default="QUEUEING")
