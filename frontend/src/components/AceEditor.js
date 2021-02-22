be9dc4c08cda7d7c4b11da6c846aa0eaeeae5817
Matias Borghi <borghi.matias@gmail.com>
2021-02-22 16:44:44 -0300

    improved placeholder message:

--- frontend/src/components/AceEditor.js
+++ frontend/src/components/AceEditor.js
@@ -7,10 +7,29 @@ import "brace/ext/language_tools";
 import "brace/ext/searchbox";
 
 export function JuliaEditor({ code, setCode }) {
+    let placeholder = `
+    Write the Julia code you want to run.
 
+    An example code is the following:
+    
+    using Statistics
+    using DifferentialEquations
+    
+    α=1
+    β=1
+    u₀=1/2
+    f(u,p,t) = α*u
+    g(u,p,t) = β*u
+    dt = 1//2^(4)
+    tspan = (0.0,1.0)
+    prob = SDEProblem(f,g,u₀,(0.0,1.0))
+    ensembleprob = EnsembleProblem(prob)
+    sol = solve(ensembleprob; trajectories=1000)
+    mean(sol[:, end])
+    `
     return (
         <AceEditor
-            placeholder="Write Julia Code"
+            placeholder={placeholder}
             mode="julia"
             theme="github"
             name="blah2"
