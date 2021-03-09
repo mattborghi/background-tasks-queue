0263f4fa302e28074fe3db2286684faaca1dbce1
Matias Borghi <borghi.matias@gmail.com>
2021-03-09 15:50:52 -0300

    fixed bug in button name

--- frontend/src/components/TopButtons.js
+++ frontend/src/components/TopButtons.js
@@ -1,6 +1,6 @@
 
 import { makeStyles } from '@material-ui/core/styles';
-import { AddIcon } from '@material-ui/data-grid';
+import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 
